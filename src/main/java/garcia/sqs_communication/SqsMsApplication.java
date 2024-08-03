7984a14668ca45acf3ba8469effbfb277d2f5cfe
ignacio <ign.garcia61@gmail.com>
2024-08-03 17:54:16 -0300

    initial commit

---
+++ src/main/java/garcia/sqs_communication/SqsMsApplication.java
@@ -0,0 +1,13 @@
+package garcia.sqs_communication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqsMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqsMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/garcia/sqs_communication/SqsMsApplicationTests.java
@@ -0,0 +1,13 @@
+package garcia.sqs_communication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqsMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
